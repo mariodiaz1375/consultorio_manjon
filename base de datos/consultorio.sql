6a1940c58cb57a91168a668940097aed5b116617
mario <mariodiaz1375@gmail.com>
2025-06-06 12:20:57 -0300

    se editaron los pagos

--- base de datos/consultorio.sql
+++ base de datos/consultorio.sql
@@ -37,13 +37,21 @@ nom_trat VARCHAR(50) NOT NULL,
 desc_trat VARCHAR(100),
 CONSTRAINT pk_trat PRIMARY KEY(id_trat));
 
-CREATE TABLE pagos (
-id_pago INT AUTO_INCREMENT,
-id_cuota INT,
-id_entrega INT,
+CREATE TABLE historia_clinica (
+id_hc INT AUTO_INCREMENT,
+id_paciente_hc INT NOT NULL,
+desc_hc VARCHAR(100),
+finalizado BOOLEAN,
 pagado BOOLEAN,
-fecha_limite DATE,
-fecha_pago TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+fecha_inicio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+fecha_fin DATE);
+
+CREATE TABLE trat_pd_cd (
+id_trat_pd_cd INT AUTO_INCREMENT,
+id_trat INT NOT NULL,
+id_cd INT NOT NULL,
+id_pd INT NOT NULL,
+id_hc INT NOT NULL);
 
 CREATE TABLE entregas (
 id_entrega INT AUTO_INCREMENT,
@@ -53,22 +61,13 @@ CREATE TABLE cuotas (
 id_cuota INT AUTO_INCREMENT,
 nom_cuota VARCHAR(20));
 
-CREATE TABLE historia_clinica (
-id_hc INT AUTO_INCREMENT,
-id_paciente_hc INT NOT NULL,
-desc_hc VARCHAR(100),
-finalizado BOOLEAN,
+CREATE TABLE pagos (
+id_pago INT AUTO_INCREMENT,
+id_cuota INT,
+id_entrega INT,
 pagado BOOLEAN,
-fecha_inicio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-fecha_fin TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
-
-CREATE TABLE trat_pd_cd (
-id_trat_pd_cd INT AUTO_INCREMENT,
-id_trat INT NOT NULL,
-id_cd INT NOT NULL,
-id_pd INT NOT NULL,
-id_hc INT NOT NULL);
-
+fecha_limite DATE,
+fecha_pago DATETIME);
 
 -- AGREGANDO CONSTRAINT A LAS TABLAS CON FORANEAS
 ALTER TABLE odon_esp 
@@ -82,4 +81,28 @@ FOREIGN KEY (id_esp) REFERENCES especialidades(id_esp);
 ALTER TABLE historia_clinica
 ADD CONSTRAINT fk_paciente
 FOREIGN KEY (id_paciente_hc) REFERENCES pacientes(id_pac);
+
+ALTER TABLE trat_pd_cd
+ADD CONSTRAINT fk_trat
+FOREIGN KEY (id_trat) REFERENCES tratamientos(id_trat);
+
+ALTER TABLE trat_pd_cd
+ADD CONSTRAINT fk_cd
+FOREIGN KEY (id_cd) REFERENCES tratamientos(id_cara);
+
+ALTER TABLE trat_pd_cd
+ADD CONSTRAINT fk_pd
+FOREIGN KEY (id_pd) REFERENCES tratamientos(id_pieza);
+
+ALTER TABLE trat_pd_cd
+ADD CONSTRAINT fk_hc
+FOREIGN KEY (id_hc) REFERENCES tratamientos(id_hc);
+
+ALTER TABLE pagos
+ADD CONSTRAINT fk_cuota
+FOREIGN KEY (id_cuota) REFERENCES cuotas(id_cuota);
+
+ALTER TABLE pagos
+ADD CONSTRAINT fk_entrega
+FOREIGN KEY (id_entrega) REFERENCES entegas(id_entrega);
 
