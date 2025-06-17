b39a432a31fe1ed72e5f82a9d27f584bca5d0b4f
Mario <mariodiaz1375@gmail.com>
2025-06-16 23:21:33 -0300

    muchas inserciones, funcionando

--- base de datos/consultas.sql
+++ base de datos/consultas.sql
@@ -1,8 +1,23 @@
 SELECT * FROM pacientes;
 SELECT * FROM historia_clinica;
 
-SELECT id_pac, nom_pac, desc_hc, finalizado, pagado, fecha_inicio, fecha_fin, nom_trat, nomb_cara
-FROM pacientes INNER JOIN historia_clinica hc ON id_pac = id_paciente_hc
-INNER JOIN trat_pd_cd tpc ON id_hc.hc = id_hc.tpc
-GROUP BY id_pac
-ORDER BY id_pac;
+SELECT id_pac, nom_pac, desc_hc, finalizado, fecha_inicio, fecha_fin, nom_trat, nomb_cara
+FROM pacientes 
+INNER JOIN historia_clinica hc ON id_pac = id_paciente_hc
+INNER JOIN trat_pd_cd tpc ON hc.id_hc = tpc.id_hc
+INNER JOIN piezas_dentales ON id_pieza = id_pd
+INNER JOIN tratamientos trat ON trat.id_trat = tpc.id_trat
+INNER JOIN caras_dentales cd ON id_cara = id_cd
+WHERE dni_pac = "27555666";
+
+select * from pacientes;
+
+select * from historia_clinica where id_paciente_hc = 2;
+
+select hc.id_hc, desc_hc, fecha_inicio, fecha_fin, nom_trat, nomb_cara
+from historia_clinica hc
+INNER JOIN trat_pd_cd tpc ON hc.id_hc = tpc.id_hc
+INNER JOIN piezas_dentales ON id_pieza = id_pd
+INNER JOIN tratamientos trat ON trat.id_trat = tpc.id_trat
+INNER JOIN caras_dentales cd ON id_cara = id_cd
+WHERE hc.id_hc = 2;
